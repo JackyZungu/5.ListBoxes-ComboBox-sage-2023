67676caa13e8e2c704da8d9b2ac3c60d16b3272b
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-03 14:47:03 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        ComboBox1 = New ComboBox()
+        SuspendLayout()
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 20
+        ListBox1.Items.AddRange(New Object() {"iPhone ", "Huawei", "Samsung", "Sony", "Mobicel"})
+        ListBox1.Location = New Point(12, 8)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(124, 424)
+        ListBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(239, 46)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(140, 29)
+        Button1.TabIndex = 1
+        Button1.Text = "Add Item"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(239, 98)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(140, 29)
+        Button2.TabIndex = 2
+        Button2.Text = "Remove"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(242, 151)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(137, 29)
+        Button3.TabIndex = 3
+        Button3.Text = "Delete Select"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(242, 207)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(137, 29)
+        Button4.TabIndex = 4
+        Button4.Text = "Clear"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(474, 46)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ComboBox1)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ListBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'ListBox1.Items.Add("Redmi")
+        Dim myitem
+
+        'myitem = InputBox("Enter your fav Phone")
+        'istBox1.Items.Add(myitem)
+
+        myitem = InputBox("Enter your Phone")
+        ComboBox1.Items.Add(myitem)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'ListBox1.Items.Remove("Mobicel")
+        Dim myitem2
+        'myitem2 = InputBox("Enter name of the phone to reamove")
+        'ListBox1.Items.Remove(myitem2)
+        myitem2 = InputBox("Enter name of the phone to reamove")
+        ComboBox1.Items.Remove(myitem2)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'ListBox1.Items.Remove(ListBox1.SelectedItem)
+        ComboBox1.Items.Remove(ComboBox1.SelectedItem)
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'ListBox1.Items.Clear()
+        ComboBox1.Items.Clear()
+    End Sub
+End Class
